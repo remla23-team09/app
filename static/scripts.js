df3d084a91048cb7a6f2eda081fe012573396362
Krzysztof Garbowicz <kgar@netcompany.com>
2023-05-08 14:23:30 +0200

    Add logging

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,7 @@ function analyze() {
     $("#result").text(""); // Clear the old message
     const review = $("#review").val();
     $.post("/analyze", {review: review}, function (data) {
+        console.log(data);
         $("#result").text(data.prediction > "0" ? "😊" : "☹️");
     }).fail(function () {
         $("#result").text("Error");
