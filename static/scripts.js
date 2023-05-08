ed5873cd58085acc8aee2142c9d59a881dad179a
Krzysztof Garbowicz <kgar@netcompany.com>
2023-05-08 14:17:32 +0200

    Update comparison

--- static/scripts.js
+++ static/scripts.js
@@ -2,7 +2,7 @@ function analyze() {
     $("#result").text(""); // Clear the old message
     const review = $("#review").val();
     $.post("/analyze", {review: review}, function (data) {
-        $("#result").text(parseInt(data.prediction) > 0 ? "😊" : "☹️");
+        $("#result").text(data.prediction > "0" ? "😊" : "☹️");
     }).fail(function () {
         $("#result").text("Error");
     });
