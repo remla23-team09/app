e64ba458f9efae200788b703cb57fca5101ed217
Krzysztof Garbowicz <kgar@netcompany.com>
2023-05-29 21:42:32 +0200

    dynamically change image tag; add metrics

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import os
 import requests
 from test_package_15551 import cookie2dict
+from prometheus_client import Counter, Gauge, Histogram, Summary, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
 
@@ -10,12 +12,24 @@ MODEL_SERVICE_URL = os.environ.get("MODEL_HOST", "http://localhost:8081")
 
 history = []
 
+button_counter = Counter("button_counter", "Count the number of button presses.")
+# time_individual = Gauge("gauge_time", "Count the duration for different steps.", ["step"])
+# size_of_input = Histogram("histogram_size_of_input", "The number of characters in the input.", buckets=[0, 5, 10, 15, 20, 25, 50, 75, 100])
+# time_summary = Summary("summary_time", "Summarizing duration for different steps", ["step"])
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+
 @app.route("/")
 def home():
     return render_template('index.html', history=history)
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
+    global button_counter
+
+    button_counter.inc()
     
     review = request.form.get("review")
     restaurant = request.form.get('restaurant')
